ec2e32be344e3ed00ed41293354ecdfe891e5319
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-08-31 18:56:06 -0300

    Otimizando a tipagem das permissões

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,20 +2,20 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Roles = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
 ) => void
 
-export const permissions: Record<Roles, PermissionByRole> = {
-  ADMIN: (user, { can }) => {
+export const permissions: Record<Role, PermissionByRole> = {
+  ADMIN: (_, { can }) => {
     can('manage', 'all')
   },
-  MEMBER: (user, { can }) => {
-    can('invite', 'User')
+  MEMBER: (_, { can }) => {
+    // can('invite', 'User')
     can('manage', 'Project')
   },
+  BILLING: () => {},
 }
