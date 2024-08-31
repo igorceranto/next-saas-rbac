6b707b50b714ee8789223f4401735c5569d99941
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-08-31 13:05:41 -0300

    CASL -> Criando primeiras permissões

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse) // true
+console.log(userCanDeleteOtherUsers) // false
+console.log(userCannotDeleteOtherUsers) // true
