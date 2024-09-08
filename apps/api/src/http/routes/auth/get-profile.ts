672042ed87cef2b0ffdd388e45feac3d8b3c4890
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-09-07 22:28:30 -0300

    Criando middleware de autenticação

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -2,51 +2,55 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
+import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
 import { BadRequestError } from '../_errors/bad-request-error'
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        tags: ['Auth'],
-        summary: 'Get authenticated user profile',
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string(),
-              name: z.string().nullable(),
-              email: z.string(),
-              avatarUrl: z.string().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['Auth'],
+          summary: 'Get authenticated user profile',
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string(),
+                name: z.string().nullable(),
+                email: z.string(),
+                avatarUrl: z.string().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>()
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
 
-      const user = await prisma.user.findUnique({
-        where: {
-          id: sub,
-        },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUrl: true,
-        },
-      })
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+        })
 
-      if (!user) {
-        throw new BadRequestError('User not found')
-      }
+        if (!user) {
+          throw new BadRequestError('User not found')
+        }
 
-      reply.send({
-        user,
-      })
-    },
-  )
+        reply.send({
+          user,
+        })
+      },
+    )
 }
