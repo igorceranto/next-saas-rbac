acce35d6072f50270ba996e5318f9917eaa16aa8
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-09-19 20:30:57 -0300

    Rota: Remover membro

--- apps/api/src/http/routes/members/remove-member.ts
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -1,4 +1,3 @@
-import { roleSchema } from '@saas/auth'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
@@ -9,23 +8,20 @@ import { getUserPermissions } from '@/utils/get-user-permissions'
 
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
-export async function updateMember(app: FastifyInstance) {
+export async function removeMember(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
-    .put(
+    .delete(
       '/organizations/:slug/members/:memberId',
       {
         schema: {
           tags: ['members'],
-          summary: 'Update a member',
+          summary: 'Remove a member from the organization',
           security: [{ bearerAuth: [] }],
           params: z.object({
             slug: z.string(),
             memberId: z.string().uuid(),
-          }),
-          body: z.object({
-            role: roleSchema,
           }),
           response: {
             204: z.null(),
@@ -41,21 +37,16 @@ export async function updateMember(app: FastifyInstance) {
 
         const { cannot } = getUserPermissions(userId, membership.role)
 
-        if (cannot('update', 'User')) {
+        if (cannot('delete', 'User')) {
           throw new UnauthorizedError(
-            `You're not allowed to update this member.`,
+            `You're not allowed to remove this member from the organization.`,
           )
         }
 
-        const { role } = request.body
-
-        await prisma.member.update({
+        await prisma.member.delete({
           where: {
             id: memberId,
             organizationId: organization.id,
-          },
-          data: {
-            role,
           },
         })
 
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,7 +19,8 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { passwordReset } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
-import { updateMembers } from './routes/members/update-member'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -90,7 +91,8 @@ app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
 app.register(getMembers)
-app.register(updateMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running 🔥')
