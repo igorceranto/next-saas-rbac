3b75eaf1526b399aef1893c81d68ad6a815b461e
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-09-20 00:02:48 -0300

    Páginas de cadastro e recuperação

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className={`${roboto.className} dark`}>
       <body>{children}</body>
     </html>
   )
