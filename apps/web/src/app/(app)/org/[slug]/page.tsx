ea7a2194764bb2139a933ff29f46e47e60676aba
Igor Ceranto <31713934+igorceranto@users.noreply.github.com>
2024-10-20 20:35:31 -0300

    Exibindo organização ativa

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/Header'
+
+export default async function Projects() {
+  return (
+    <div className="py-4">
+      <Header />
+      <main></main>
+    </div>
+  )
+}
--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,4 +1,5 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 import { getOrganization } from '@/http/get-organization'
@@ -15,12 +16,32 @@ import {
 } from './ui/dropdown-menu'
 
 export async function OrganizationSwitcher() {
+  const correntOrg = cookies().get('org')?.value
+
   const { organizations } = await getOrganization()
+
+  const currentOrganization = organizations.find(
+    (org) => org.slug === correntOrg,
+  )
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
-        <span className="text-muted-foreground">Select Organization</span>
+        {currentOrganization ? (
+          <>
+            <Avatar className="mr-2 size-4">
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback />
+            </Avatar>
+            <span className="truncate text-left">
+              {currentOrganization.name}
+            </span>
+          </>
+        ) : (
+          <span className="text-muted-foreground">Select Organization</span>
+        )}
         <ChevronsUpDown className="ml-auto size-4 text-muted-foreground" />
       </DropdownMenuTrigger>
       <DropdownMenuContent
